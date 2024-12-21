e86741c9a818aa5857469b484f47217ae241431b
Jasper <martinjp13579@gmail.com>
2024-12-21 16:01:15 +0800

    Add documents app and Document model

---
+++ documents/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        from ..search_manager import SearchManager
+        search_manager = SearchManager()
+        search_manager.add_document(self.id, self.title, self.content)
