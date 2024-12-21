d5fce8019329fe2623d69ee5e7bd69f9f7fba2c6
Jasper <martinjp13579@gmail.com>
2024-12-21 23:20:59 +0800

    Initialize Django project and add dependencies

--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from .search_manager import SearchManager
+
+def search_documents(request):
+    query = request.GET.get("q", "")
+    if not query:
+        return JsonResponse({"error": "Query string is empty"}, status=400)
+
+    search_manager = SearchManager()
+    results = search_manager.search(query)
+    return render(request, 'search_results.html', {'query': query, 'results': results})
